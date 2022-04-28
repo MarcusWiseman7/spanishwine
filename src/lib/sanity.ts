7982f721223a7061944934be5ec5e2cd3d179822
Marcus Wiseman <md.wiseman@hotmail.com>
2022-04-28 17:36:23 +0200

    Sanity project

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 const sanity = sanityClient({
-    projectId: '',
+    projectId: 'ia3gbp76',
     dataset: 'production',
     apiVersion: '2021-10-21',
     useCdn: true,
